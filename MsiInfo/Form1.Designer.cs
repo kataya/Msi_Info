5fd5e6574dadb137d3ffbcb6b4c497961aae3499
kataya <shinji_kataya@esrij.com>
2018-07-11 21:39:17 +0900

    Msi,Mspのアンインストールコマンド取得の.NETサンプルコード

---
+++ MsiInfo/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace MsiInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(479, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ファイル選択";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "アンインストール コマンド例：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MSI ：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "> mxiexec /X <PruductGUID>";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "MSP ：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(67, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(334, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "> msiexec /I <ProductGUID> MSIPATCHREMOVE=<PatchGUID>";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(461, 88);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(12, 106);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(461, 77);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 195);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "MSI や MSP のアンインストール コマンド取得";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MsiInfo/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInstaller;
+
+namespace MsiInfo
+{
+
+    /// <summary>
+    /// インストールやアンインストールのときに必要な MSI や MSPの情報を[orca]なしで確認するためのサンプル
+    /// ・PowerShellでの実装
+    ///      https://marcinotorowski.com/2018/03/04/enumerating-installed-msi-products-with-powershell/
+    /// ・pinvoke:: DLL importでの実装
+    ///      https://www.neowin.net/forum/topic/968772-vbnet-msi-manipulation/
+    /// </summary>
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog fld = new OpenFileDialog();
+            fld.InitialDirectory = Environment.CurrentDirectory;
+            fld.Filter = "MSI ファイル (*.msi)|*.msi|MSP ファイル (*.msp)|*.msp";
+
+            if (fld.ShowDialog() == DialogResult.OK) {
+                string selfile = fld.FileName;
+                string ext = System.IO.Path.GetExtension(selfile);
+                string info = string.Empty;
+                if (ext.Equals(".msi")){
+                    //ProductName, ProductCode, UpgradeCode, Manufacturer, ARPHELPLINK, ARPCOMMENTS, ARPCONTACT, ARPURLINFOABOUT and ARPURLUDATEINFO
+                    info = GetMsiProperty(selfile, "ProductCode");
+                }
+                else if (ext.Equals(".msp")) {
+                    info = GetMspSummaryInfo(selfile);
+                }
+
+                this.textBox1.Text = info;
+                //MessageBox.Show(this, info);
+            }
+
+
+        }
+
+        /// <summary>
+        /// MSP ファイルからアンインストール時に必要な情報を取得
+        /// </summary>
+        /// <param name="mspFile"></param>
+        /// <returns></returns>
+        public static string GetMspSummaryInfo(string mspFile)
+        {
+            string retVal = string.Empty;
+
+            Type classType = Type.GetTypeFromProgID("WindowsInstaller.Installer");
+            Object installerObj = Activator.CreateInstance(classType);
+            Installer installer = installerObj as Installer;
+
+            // 読み込みのために msp ファイルをオープン
+            // 0 - Read, 1 - Read/Write
+            Database database = installer.OpenDatabase(mspFile, 32);
+
+            int propCnt = database.SummaryInformation.PropertyCount;
+
+            // orca での 「Patch Summary Information」画面 
+            //retVal = string.Format("Targets: {0}", database.SummaryInformation.Property[7]); // [7]は Targets:
+
+            //retVal += " , " + string.Format("Patch Code: {0}", database.SummaryInformation.Property[9]); // [9]は Patch Code:
+
+            // アンインストール用のコマンドの形式で返す
+            // msiexec / I < ProductGUID > MSIPATCHREMOVE =< PatchGUID >
+            retVal = string.Format("msiexec /I '{0}' MSIPATCHREMOVE='{1}'", database.SummaryInformation.Property[7], database.SummaryInformation.Property[9]);
+            retVal = retVal.Replace("'", "\"");
+
+            return retVal;
+        }
+
+
+        /// <summary>
+        /// Msi ファイルからアンインストール時に必要な情報を取得
+        /// 参考1: Read Properties from an MSI File
+        ///   http://www.alteridem.net/2008/05/20/read-properties-from-an-msi-file/
+        /// 
+        /// 参考2: COM Microsoft Windows Installer Object Library を利用とあるが参照しようとするとエラー
+        ///   %WINDIR%\system32\msi.dll をかわりに追加するといい
+        ///   http://www.reinholdt.me/2015/07/29/problem-adding-a-c-reference-to-windowsinstaller-com-object/
+        /// </summary>
+        /// <param name="msiFile"></param>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public static string GetMsiProperty(string msiFile, string property)
+        {
+            string retVal = string.Empty;
+
+
+            // インストーラのインスタンスを作成
+            Type classType = Type.GetTypeFromProgID("WindowsInstaller.Installer");
+            Object installerObj = Activator.CreateInstance(classType);
+            Installer installer = installerObj as Installer;
+
+            // 読み込みのために msi ファイルをオープン
+            // 0 - Read, 1 - Read/Write
+            Database database = installer.OpenDatabase(msiFile,0);
+
+            // SQL文でプロパティをクエリして取得
+            string sql = String.Format(
+                "SELECT Value FROM Property WHERE Property ='{0}'", property);
+            WindowsInstaller.View view = database.OpenView(sql);
+            view.Execute(null);
+
+            // 取得したレコードの読み込み
+            Record record = view.Fetch();
+            if (record != null)
+                //retVal = string.Format("{0}: {1}", property, record.get_StringData(1));
+                // アンインストール用のコマンドの形式で返す
+                // mxiexec /X <PruductGUID>
+                retVal = string.Format("mxiexec /X '{0}'", record.get_StringData(1));
+                retVal = retVal.Replace("'", "\"");
+
+            return retVal;
+        }
+
+
+    }
+}
